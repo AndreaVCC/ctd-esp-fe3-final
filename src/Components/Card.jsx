b3353ee0538fb57abe27e62fb1093f289629d657
Andrea Carreño <andrea.carreno@meetcard.cl>
2024-09-11 22:31:06 -0300

    CHG:cmabios iconos corrección fav y card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({ name, username, id, onUpdate }) => {
-  const [isFav, setIsFav] = useState(false);
+const Card = ({ name, username, id, onUpdate, isFav: initialIsFav }) => {
+  const [isFav, setIsFav] = useState(initialIsFav || false);
 
   useEffect(() => {
-    const storedFavs = JSON.parse(localStorage.getItem("favorites")) || [];
-    const isFavorite = storedFavs.some((fav) => fav.id === id);
-    setIsFav(isFavorite);
-  }, [id]);
+    if (initialIsFav === undefined) { // Solo ejecutar este efecto si no se pasa el prop `isFav`
+      const storedFavs = JSON.parse(localStorage.getItem("favorites")) || [];
+      const isFavorite = storedFavs.some((fav) => fav.id === id);
+      setIsFav(isFavorite);
+    }
+  }, [id, initialIsFav]);
+
 
   const addFav = () => {
     const dentist = { id, name, username };
@@ -38,11 +41,12 @@ const Card = ({ name, username, id, onUpdate }) => {
       />
       <h2>{name}</h2>
       <p>{username}</p>
-      <Link to={`/dentist/${id}`}>View Details</Link>
-      {/* El texto del botón cambia dinámicamente */}
-      <button onClick={addFav} className="favButton">
-        {isFav ? "Remove fav" : "Add fav"}
-      </button>
+      <Link to={`/dentist/${id}`}>Ver detalle</Link>
+      {isFav ? (
+        <img src="./images/icon-fav.png" alt="" onClick={addFav} />
+      ) : (
+        <img src="./images/icon-nofav.png" alt="" onClick={addFav} />
+      )}
     </div>
   );
 };
--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -16,28 +16,31 @@ const Favs = () => {
     setFavorites(storedFavs); // Actualiza el estado local
   };
 
-  return (
-    <main>
-      <h1>Dentists Favs</h1>
-      <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
-        {favorites.length > 0 ? (
-          favorites.map((dentist) => (
-            <Card
-              key={dentist.id}
-              name={dentist.name}
-              username={dentist.username}
-              id={dentist.id}
-              onUpdate={updateFavs} // actulizar eliminar
-            />
-          ))
-        ) : (
-          <p>No favorites found</p>
-        )}
-      </div>
-    </main>
-  );
+    return (
+      <main>
+        <h1>Dentists Favs</h1>
+        <div className="card-grid">
+          {/* este componente debe consumir los destacados del localStorage */}
+          {/* Deberan renderizar una Card por cada uno de ellos */}
+          {favorites.length > 0 ? (
+            favorites.map((dentist) => (
+              <Card
+                key={dentist.id}
+                name={dentist.name}
+                username={dentist.username}
+                id={dentist.id}
+                isFav={true} 
+                onUpdate={updateFavs} // actulizar eliminar
+              />
+            ))
+          ) : (
+            <p>No favorites found</p>
+          )}
+        </div>
+      </main>
+    );
+    
+  
 };
 
 export default Favs;
